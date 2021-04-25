f521c3758624ebcce9fd39b6b31f0e1e6df8d0d5
unknown <monica.winnie3@gmail.com>
2021-04-25 14:27:30 +0700

    project piranti

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,7 @@ use App\Http\Controllers\ProductMutationController;
 | contains the "web" middleware group. Now create something great!
 |
 */
+
 Route::get('', function() {
     return redirect()->route('product.index');
 });
